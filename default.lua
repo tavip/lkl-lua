7364317c18db2f2371fc227bb370e64f1a816383
Lucian Adrian Grijincu <Lucian.Grijincu@gmail.com>
2007-12-10 04:56:11 +0200

    test for the new stat

--- default.lua
+++ default.lua
@@ -1,22 +1,24 @@
 
 print ("~~~~~~~~~~~~~~ begin lua code. Respect my authoritah!")
 filename = "default.lua"
-a,v = stat(filename)
+a,v = stat(filename, "type", "size")
 print (a,v)
-a = lkl.dir("/")
-
-t = {}
-print("before insert");
-for i in a do
-    table.insert(t,i)
+a = lkl.dir(1)
+if a == nil then
+    print "suck to be a nilval"
+else
+    t = {}
+    print("before insert");
+    for i in a do
+        table.insert(t,i)
+    end
+    print("before sort");
+    table.sort(t)
+    
+    for i,v in ipairs(t) do
+        print (i,v)
+    end
 end
-print("before sort");
-table.sort(t)
-
-for i,v in ipairs(t) do
-    print (i,v)
-end
-
 a,b = lkl.get_pid()
 print ("a=", a, "b=", b)
 print ("~~~ end of the world")
