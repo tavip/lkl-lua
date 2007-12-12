a987366f2fa1f8e9fbcbcb03d9b842e466d556a4
Lucian Adrian Grijincu <Lucian.Grijincu@gmail.com>
2007-12-12 12:16:18 +0200

    added testcase for the new file_rename and file_copy

--- default.lua
+++ default.lua
@@ -1,14 +1,19 @@
-
+pwd = "/home/gringo/ix/lualkl/"
 print ("~~~~~~~~~~~~~~ begin lua code. Respect my authoritah!")
 filename = "default.lua"
-a = lkl.stat(filename, "size", "type")
+a = lkl.stat(filename)
 print (a["size"])
 for i,v in pairs(a) do
     print (i,v)
 end
 print ("after printing the table")
 
-
+ret = lkl.file_rename(pwd .. "test1", pwd .. "test2")
+print("file_rename returned", ret)
+ret = lkl.file_rename(pwd .. "test2", pwd .. "test1");
+print("file_rename returned", ret)
+ret = lkl.file_copy("test1", "copydest");
+print("file_copy returned", ret)
 a = lkl.dir(1)
 if a == nil then
     print "suck to be a nilval"
