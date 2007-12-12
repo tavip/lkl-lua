698296c0448033404be82dfc515b9cf146253e3a
Lucian Adrian Grijincu <Lucian.Grijincu@gmail.com>
2007-12-12 12:15:14 +0200

    added a wapr_file_copy routine

--- wapr_fileops.c
+++ wapr_fileops.c
@@ -648,4 +648,82 @@ apr_status_t wapr_file_unlock(wapr_file_t *thefile)
 	return APR_SUCCESS;
 }
 
+static apr_status_t wapr_file_transfer_contents(const char *from_path,
+                                                const char *to_path,
+                                                apr_int32_t flags,
+                                                apr_fileperms_t to_perms,
+                                                apr_pool_t *pool)
+{
+    wapr_file_t *s, *d;
+    apr_status_t status;
+    apr_finfo_t finfo;
+    apr_fileperms_t perms;
+
+    /* Open source file. */
+    status = wapr_file_open(&s, from_path, APR_READ, APR_OS_DEFAULT, pool);
+    if (status)
+        return status;
+
+    /* Maybe get its permissions. */
+    if (to_perms == APR_FILE_SOURCE_PERMS) {
+        status = wapr_file_info_get(&finfo, APR_FINFO_PROT, s);
+        if (status != APR_SUCCESS && status != APR_INCOMPLETE) {
+            wapr_file_close(s);  /* toss any error */
+            return status;
+        }
+        perms = finfo.protection;
+    }
+    else
+        perms = to_perms;
+
+    /* Open dest file. */
+    status = wapr_file_open(&d, to_path, flags, perms, pool);
+    if (status) {
+        wapr_file_close(s);  /* toss any error */
+        return status;
+    }
+
+    /* Copy bytes till the cows come home. */
+    while (1) {
+        char buf[BUFSIZ];
+        apr_size_t bytes_this_time = sizeof(buf);
+        apr_status_t read_err;
+        apr_status_t write_err;
+
+        /* Read 'em. */
+        read_err = wapr_file_read(s, buf, &bytes_this_time);
+        if (read_err && !APR_STATUS_IS_EOF(read_err)) {
+            wapr_file_close(s);  /* toss any error */
+            wapr_file_close(d);  /* toss any error */
+            return read_err;
+        }
+
+        /* Write 'em. */
+        write_err = wapr_file_write_full(d, buf, bytes_this_time, NULL);
+        if (write_err) {
+            wapr_file_close(s);  /* toss any error */
+            wapr_file_close(d);  /* toss any error */
+            return write_err;
+        }
+
+        if (read_err && APR_STATUS_IS_EOF(read_err)) {
+            status = wapr_file_close(s);
+            if (status) {
+                wapr_file_close(d);  /* toss any error */
+                return status;
+            }
+
+            /* return the results of this close: an error, or success */
+            return wapr_file_close(d);
+        }
+    }
+    /* NOTREACHED */
+}
+
+
+
+apr_status_t wapr_file_copy(const char * source, const char * dest, apr_fileperms_t perms, apr_pool_t *pool)
+{
+    return wapr_file_transfer_contents(source, dest, (APR_WRITE | APR_CREATE | APR_TRUNCATE), perms, pool);
+}
 #endif
